052c1f39eb052905a57b05bf6bc955a49e901c7a
akilika <angelikaakinina05@gmail.com>
2025-02-12 18:11:21 +0300

    Homework Fix

--- src/Author.java
+++ src/Author.java
@@ -1,17 +1,23 @@
 public class Author {
 
     private String name;
+    private String lastName;
 
-    public Author(String name) {
+    public Author(String lastName, String name) {
         this.name = name;
+        this.lastName = lastName;
     }
 
     public String getName() {
         return this.name;
     }
 
+    public String getLastName() {
+        return this.lastName;
+    }
+
     public String toString() {
-        return name;
+        return lastName + " " + name;
     }
 
 }
--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,13 @@
 public class Main {
     public static void main(String[] args) {
 
-        Author author = new Author("Джером Сэинджер");
-        Author author1 = new Author("Мариам Петросян");
+        Author author = new Author("Джером", "Сэинджер");
+        Author author1 = new Author("Мариам", "Петросян");
         Book book = new Book(author, "Над пропастью во ржи", 1951);
         Book book1 = new Book(author1, "Дом в котором", 2009);
 
-        System.out.println("author.getName() = " + author.getName());
-        System.out.println("author1.getName() = " + author1.getName());
+        System.out.println("author = " + author);
+        System.out.println("author1 = " + author1);
 
         System.out.println("book = " + book);
         System.out.println("book1 = " + book1);
